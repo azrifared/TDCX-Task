367503cb6076d3554122365cb5aa82aa7506c4c8
Azri Farid Shahroom (DHL IT Services) <azrifarid.shahroom@dhl.com>
2020-11-01 00:26:34 +0800

    Updates

--- bundle.js
+++ bundle.js
@@ -19,7 +19,7 @@
 /*! CommonJS bailout: this is used directly at 2:23-27 */
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nexports.App = void 0;\r\nvar react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\r\nexports.App = function (_a) {\r\n    var userName = _a.userName, lang = _a.lang;\r\n    return (react_1.default.createElement(\"h1\", null,\r\n        \"Hi \",\r\n        userName,\r\n        \" from React! Welcome to \",\r\n        lang,\r\n        \"!\"));\r\n};\r\n\n\n//# sourceURL=webpack://TDCX-Task/./src/App.tsx?");
+eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nvar react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\r\nexports.default = (function () { return (react_1.default.createElement(\"h1\", null, \"Waiting for the task!\")); });\r\n\n\n//# sourceURL=webpack://TDCX-Task/./src/App.tsx?");
 
 /***/ }),
 
@@ -30,11 +30,9 @@ eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod)
 /*! unknown exports (runtime-defined) */
 /*! runtime requirements: top-level-this-exports, __webpack_exports__, __webpack_require__ */
 /*! CommonJS bailout: this is used directly at 2:23-27 */
-/*! CommonJS bailout: this is used directly at 9:26-30 */
-/*! CommonJS bailout: this is used directly at 14:20-24 */
 /***/ (function(__unused_webpack_module, exports, __webpack_require__) {
 
-eval("\r\nvar __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });\r\n}) : (function(o, m, k, k2) {\r\n    if (k2 === undefined) k2 = k;\r\n    o[k2] = m[k];\r\n}));\r\nvar __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {\r\n    Object.defineProperty(o, \"default\", { enumerable: true, value: v });\r\n}) : function(o, v) {\r\n    o[\"default\"] = v;\r\n});\r\nvar __importStar = (this && this.__importStar) || function (mod) {\r\n    if (mod && mod.__esModule) return mod;\r\n    var result = {};\r\n    if (mod != null) for (var k in mod) if (k !== \"default\" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);\r\n    __setModuleDefault(result, mod);\r\n    return result;\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nvar React = __importStar(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\r\nvar ReactDOM = __importStar(__webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\"));\r\nvar App_1 = __webpack_require__(/*! ./App */ \"./src/App.tsx\");\r\nReactDOM.render(React.createElement(App_1.App, { userName: \"Beveloper\", lang: \"TypeScript\" }), document.getElementById(\"output\"));\r\n\n\n//# sourceURL=webpack://TDCX-Task/./src/index.tsx?");
+eval("\r\nvar __importDefault = (this && this.__importDefault) || function (mod) {\r\n    return (mod && mod.__esModule) ? mod : { \"default\": mod };\r\n};\r\nObject.defineProperty(exports, \"__esModule\", ({ value: true }));\r\nvar react_1 = __importDefault(__webpack_require__(/*! react */ \"./node_modules/react/index.js\"));\r\nvar react_dom_1 = __importDefault(__webpack_require__(/*! react-dom */ \"./node_modules/react-dom/index.js\"));\r\nvar App_1 = __importDefault(__webpack_require__(/*! ./App */ \"./src/App.tsx\"));\r\nreact_dom_1.default.render(react_1.default.createElement(App_1.default, null), document.getElementById(\"output\"));\r\n\n\n//# sourceURL=webpack://TDCX-Task/./src/index.tsx?");
 
 /***/ }),
 
